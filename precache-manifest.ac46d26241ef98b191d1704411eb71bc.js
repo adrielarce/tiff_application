1fed7d06907137333b18c50634dfed52c5d87165
adrielarce <aarcedelacruz@gmail.com>
2019-05-18 01:44:18 -0400

    Updates

---
+++ precache-manifest.ac46d26241ef98b191d1704411eb71bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db737405863daa897568547d8943b225",
+    "url": "/tiff_application/index.html"
+  },
+  {
+    "revision": "af98cc5e16ca6b182052",
+    "url": "/tiff_application/static/css/main.062acb4e.chunk.css"
+  },
+  {
+    "revision": "214efbc2993fbfe0c010",
+    "url": "/tiff_application/static/js/2.5f7ff4b1.chunk.js"
+  },
+  {
+    "revision": "af98cc5e16ca6b182052",
+    "url": "/tiff_application/static/js/main.6fbda0d7.chunk.js"
+  },
+  {
+    "revision": "910ddcc57fdb6064ddbd",
+    "url": "/tiff_application/static/js/runtime~main.0fa63023.js"
+  }
+]);
